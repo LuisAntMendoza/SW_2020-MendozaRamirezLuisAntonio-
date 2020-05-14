4364e967a1cba079052bd31b618f1a5f0e2c071b
LuisAntMendoza <mendozaluis@comunidad.unam.mx>
2020-05-14 13:01:49 -0500

    FEAT: se agrego el archivo actividad2.php

---
+++ actividad2.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Luis Antonio<br>
+      Dos cosas que he aprendido en el curso han sido a trabajar en equipo ya que diversas actividades han sido en este formato y a desarrollar la capacidad de hacer varias tareas a la vez ya que debido a la contingencia las clases son en linea y es dificil poner atencion a la clase mientras tratas de replicar lo que se esta haciendo.
+";
+
+?>
